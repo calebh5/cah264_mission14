c72630b676714e99dfddc0141dda0ab1e118ac2b
Caleb Hefner <hefnerca5@gmail.com>
2023-04-04 10:26:13 -0600

    Double check

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Home(props: any) {
+  return (
+    <>
+      <div>
+        <div className="row">
+          <div style={{ textAlign: 'center' }}>
+            <br></br>
+            <img src="./JoelHiltonHeadshot.jpg" alt="JoelHilton" />
+          </div>
+
+          <div style={{ textAlign: 'center' }}>
+            <h1>
+              Welcome! This is a website deticated to Joel and his site full of
+              his Movies
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/Podcasts.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function PodcastLink() {
+  return (
+    <>
+      <div>
+        <br></br>
+        <div style={{ textAlign: 'center' }}>
+          <h2>
+            <a href="https://baconsale.com/">My Podcasts</a>
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PodcastLink;
