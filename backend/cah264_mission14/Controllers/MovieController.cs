ae28aea2aa92bc2d8c485ab1a20e5ccf979f62e0
Caleb Hefner <hefnerca5@gmail.com>
2023-04-07 17:17:46 -0600

    cah_264_mission14

---
+++ backend/cah264_mission14/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using cah264_mission14.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cah264_mission14.Controllers
+{   
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private MovieDbContext context;
+
+        public MovieController(MovieDbContext temp)
+        {
+            context = temp;
+        }
+
+        public IEnumerable<MovieCollection> Get()
+        {
+            var x = context.Movies.ToArray();
+
+            return context.Movies.ToArray();
+        }
+    }
+}
---
+++ backend/cah264_mission14/Data/MovieDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cah264_mission14.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options) { }
+
+        public DbSet<MovieCollection> Movies { get; set; }
+    }
+}
